eaec8f11cf132185e0272da3f56fafa0c3a108a9
Jonathan Kim <29jkim2@gmail.com>
2025-06-09 21:42:56 -0400

    abc2

---
+++ gemini.js
@@ -0,0 +1,22 @@
+
+import { GoogleGenerativeAI } from "@google/generative-ai";
+
+const apiKey = import.meta.env.VITE_API;
+
+const genAI = new GoogleGenerativeAI(apiKey);
+
+const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
+
+
+export async function generateContent(request){
+
+    try{
+        const result = await model.generateContent(request);
+        const response = await result.response;
+        const text = await response.text();
+        return text
+    }
+    catch(err){
+        return("error");
+    }
+}
